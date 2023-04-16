6bbba0c61a57e55c7895c9e5f24e75e33ccc61ab
chernandez <crs.hdz.s@gmail.com>
2023-04-16 16:46:27 -0600

    added crud

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+import 'package:lexicon/model/user.dart';
+import 'package:lexicon/screens/edit_user.dart';
+import 'package:lexicon/screens/add_user.dart';
+import 'package:lexicon/screens/view_users.dart';
+import 'package:lexicon/services/user_service.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,41 +11,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  //  const MyApp({Key? key}) : super(key: key);
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Lexicon Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  //  const MyHomePage({Key? key}) : super(key: key);
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,66 +37,146 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  late List<User> _userList = <User>[];
+  final _userService = UserService();
+
+  getAllUserDetails() async {
+    var users = await _userService.readAllUsers();
+    _userList = <User>[];
+    users.forEach((user) {
+      setState(() {
+        var userModel = User();
+        userModel.id = user['id'];
+        userModel.name = user['name'];
+        userModel.contact = user['contact'];
+        userModel.description = user['description'];
+        _userList.add(userModel);
+      });
     });
   }
 
   @override
+  void initState() {
+    getAllUserDetails();
+    super.initState();
+  }
+
+  _showSuccessSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  _deleteFormDialog(BuildContext context, userId) {
+    return showDialog(
+        context: context,
+        builder: (param) {
+          return AlertDialog(
+            title: const Text(
+              'Are You Sure to Delete',
+              style: TextStyle(color: Colors.teal, fontSize: 20),
+            ),
+            actions: [
+              TextButton(
+                  style: TextButton.styleFrom(
+                      primary: Colors.white, // foreground
+                      backgroundColor: Colors.red),
+                  onPressed: ()  async{
+                    var result=await _userService.deleteUser(userId);
+                    if (result != null) {
+                      Navigator.pop(context);
+                      getAllUserDetails();
+                      _showSuccessSnackBar(
+                          'User Detail Deleted Success');
+                    }
+                  },
+                  child: const Text('Delete')),
+              TextButton(
+                  style: TextButton.styleFrom(
+                      primary: Colors.white, // foreground
+                      backgroundColor: Colors.teal),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Close'))
+            ],
+          );
+        });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: _userList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ViewUser(
+                            user: _userList[index],
+                          )));
+                },
+                leading: const Icon(Icons.person),
+                title: Text(_userList[index].name ?? ''),
+                subtitle: Text(_userList[index].contact ?? ''),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => EditUser(
+                                    user: _userList[index],
+                                  ))).then((data) {
+                            if (data != null) {
+                              getAllUserDetails();
+                              _showSuccessSnackBar(
+                                  'User Detail Updated Success');
+                            }
+                          });
+                          ;
+                        },
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.teal,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          _deleteFormDialog(context, _userList[index].id);
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ))
+                  ],
+                ),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const AddUser()))
+              .then((data) {
+            if (data != null) {
+              getAllUserDetails();
+              _showSuccessSnackBar('User Detail Added Success');
+            }
+          });
+        },
+        tooltip: 'Agregar',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
